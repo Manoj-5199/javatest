98f0370a3a5de8ae6c135434d448e73d15ca2bc0
Manoj-5199 <manojadhinarayanan@gmail.com>
2022-02-16 21:34:06 +0530

    added comment

--- src/com/manoj/java/test/Main.java
+++ src/com/manoj/java/test/Main.java
@@ -1,10 +1,12 @@
 package com.manoj.java.test;
 
 import java.io.IOException;
+//added comment
 
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		SeleniumTestCase tester = new SeleniumTestCase();
 		tester.case1();
+		
 	}
 }
